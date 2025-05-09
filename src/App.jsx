8451306375b70bfe9039532927557a74f12b350b
sajidmehmoodtariq30 <sajidmehmoodtariq5@gmail.com>
2025-05-09 12:22:11 +0500

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 flex justify-center items-center h-screen w-screen text-white'>Hello World</div>
+  )
+}
+
+export default App
